80de01d3c004331ee2889fdfca35d2aff90e08df
Epildofora <124823556+Epildofora@users.noreply.github.com>
2023-05-29 21:12:07 +0300

    Update StatisticsServiceImpl.java

--- src/main/java/searchengine/services/impl/StatisticsServiceImpl.java
+++ src/main/java/searchengine/services/impl/StatisticsServiceImpl.java
@@ -6,7 +6,6 @@ import searchengine.dto.statistics.DetailedStatisticsItem;
 import searchengine.dto.statistics.StatisticsData;
 import searchengine.dto.statistics.StatisticsResponse;
 import searchengine.dto.statistics.TotalStatistics;
-
 import searchengine.model.SitePage;
 import searchengine.model.Status;
 import searchengine.repository.LemmaRepository;
