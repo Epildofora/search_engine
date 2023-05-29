0cfb202f2c4b23fba69b0da7a9f69e09f8182312
Epildofora <124823556+Epildofora@users.noreply.github.com>
2023-05-29 21:08:56 +0300

    Update StatisticsSearch.java

--- src/main/java/searchengine/dto/statistics/StatisticsSearch.java
+++ src/main/java/searchengine/dto/statistics/StatisticsSearch.java
@@ -1,27 +1,11 @@
 package searchengine.dto.statistics;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Value;
 
-@Setter
-@Getter
-@NoArgsConstructor
-public class StatisticsSearch {
-    private String site;
-    private String siteName;
-    private String uri;
-    private String title;
-    private String snippet;
-    private Float relevance;
-
-    public StatisticsSearch(String site, String siteName, String uri,
-                            String title, String snippet, Float relevance) {
-        this.site = site;
-        this.siteName = siteName;
-        this.uri = uri;
-        this.title = title;
-        this.snippet = snippet;
-        this.relevance = relevance;
-    }
+@Value
+public class TotalStatistics {
+     Long sites;
+     Long pages;
+     Long lemmas;
+     boolean indexing;
 }
